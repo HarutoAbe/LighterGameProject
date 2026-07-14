8f5d59c26fa911f38ea933ffe14d337fc5b38f79
GAME\242j011 <abeharu0221@gmail.com>
2026-07-14 15:21:22 +0900

    ライターに炎が付いた

--- LighterGame/Assets/FireManager.cs
+++ LighterGame/Assets/FireManager.cs
@@ -1,8 +1,35 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-public class FireManager : MonoBehaviour
+public class FireManager : SingletonMonoBehaviour<FireManager>
 {
     [Header("‰Î‚Ì‰æ‘œ")]
-    [SerializeField] private Image fireImage = null;
+    [SerializeField] private GameObject fireImage = null;
+
+    [Header("”­‰Î‚Ì”»’è")]
+    [SerializeField] public bool isFire = false;
+
+    private void Start()
+    {
+        if (fireImage == null)
+        {
+            Debug.LogError("FireImage‚ªnull‚Å‚·I");
+        }
+        else
+        {
+            fireImage.SetActive(false);
+        }
+    }
+
+    private void Update()
+    {
+        if (isFire)
+        {
+            fireImage.SetActive(true);
+        }
+        else
+        {
+            fireImage.SetActive(false);
+        }
+    }
 }
